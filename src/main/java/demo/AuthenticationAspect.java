39ee02755059bcac1ed105002d27b13b4399de8f
Meet Furia <furia.meet@gmail.com>
2023-09-28 11:50:11 +0530

    Spring AOP Basics

---
+++ src/main/java/demo/AuthenticationAspect.java
@@ -0,0 +1,27 @@
+package demo;
+
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class AuthenticationAspect {
+
+    @Pointcut("within(demo..*)")
+    public void authenticationPointCut() {
+
+    }
+
+    @Pointcut("within(demo.ShoppingCart.*)")
+    public void authorizationPointCut() {
+
+    }
+
+    @Before("authenticationPointCut() || authorizationPointCut()")
+    public void authenticate() {
+        System.out.println("Authenticating the request");
+    }
+
+}
---
+++ src/main/java/demo/LoggingAspect.java
@@ -0,0 +1,28 @@
+package demo;
+import org.aspectj.lang.annotation.*;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class LoggingAspect {
+    @Before("execution(* demo.ShoppingCart.checkout())")
+    public void beforeLogger() {
+        System.out.println("Before Logger");
+    }
+
+    @After("execution(* demo.ShoppingCart.checkout())")
+    public void afterLogger() {
+        System.out.println("After Logger");
+    }
+
+    @Pointcut("execution(* demo.ShoppingCart.quantity(..))")
+    public void afterReturningPointCut() {
+    }
+
+    @AfterReturning(pointcut = "afterReturningPointCut()",
+    returning = "retVal")
+    public void afterReturning(String retVal) {
+        System.out.println("After returning: " + retVal);
+    }
+
+}
